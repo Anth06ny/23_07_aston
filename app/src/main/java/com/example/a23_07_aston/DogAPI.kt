2bdd331417b07ead3b90a053906d3f98dfccfdc4
AnthonyPCPortable <anth06ny@gmail.com>
2023-08-28 15:54:23 +0200

    TP Correction XMl WeatherActivity

--- app/src/main/java/com/example/a23_07_aston/DogAPI.kt
+++ app/src/main/java/com/example/a23_07_aston/DogAPI.kt
@@ -5,7 +5,7 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 
 fun main() {
-    DogAPI.getDogs().forEach { it->
+    DogAPI.getDogs().random().let {
         println("${it.breed} d'origine ${it.origin}\nNote:${it.meta.notes}\nphoto : ${it.img}")
     }
 
@@ -16,8 +16,15 @@ fun main() {
 
 object DogAPI {
 
+    fun getDogs2(): Array<DogBean> {
+        Thread.sleep(4000)
+        return arrayOf(DogBean(1, "a", "b", "c", "d", MetaBean("e", "f")))
+    }
+
     //1 méthode par appel d'API
     fun getDogs(): Array<DogBean> {
+
+
         val client = OkHttpClient()
         val request = Request.Builder()
             .url("https://dog-breeds2.p.rapidapi.com/dog_breeds")
@@ -26,23 +33,26 @@ object DogAPI {
             .addHeader("X-RapidAPI-Host", "dog-breeds2.p.rapidapi.com")
             .build()
 
-        val json = client.newCall(request).execute().use {
+        println("Avant requête")
+        val json: String = client.newCall(request).execute().use {
             //use permet de fermer la réponse qu'il y ait ou non une exception
             //Analyse du code retour
             if (!it.isSuccessful) {
+                it.close()
                 throw Exception("Réponse du serveur incorrect :${it.code}")
             }
+
             it.body.string()
         }
 
         //Convertir en objet, je lui donne l'objet de départ (ici collection)
-        val data  = Gson().fromJson(json, Array<DogBean>::class.java)
+        val data = Gson().fromJson(json, Array<DogBean>::class.java)
 
         return data
     }
 
 }
 
-data class DogBean(var id:Int, var breed:String, var origin:String, var url:String, var img:String, var meta:MetaBean)
+data class DogBean(var id: Int, var breed: String, var origin: String, var url: String, var img: String, var meta: MetaBean)
 
-data class MetaBean(var other_names:String, var notes:String)
+data class MetaBean(var other_names: String, var notes: String)
--- app/src/main/java/com/example/a23_07_aston/DogsActivity.kt
+++ app/src/main/java/com/example/a23_07_aston/DogsActivity.kt
@@ -1,9 +1,10 @@
 package com.example.a23_07_aston
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.example.a23_07_aston.databinding.ActivityDogsBinding
-import com.example.a23_07_aston.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
 
 class DogsActivity : AppCompatActivity() {
 
@@ -12,5 +13,26 @@ class DogsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.btLoad.setOnClickListener {
+            thread {
+                try {
+                    //Je vais chercher mes donénes
+                    val oneDog = DogAPI.getDogs().random()
+
+                    runOnUiThread {
+                        //Affichage
+                        binding.tvName.text = oneDog.origin
+                        binding.tvTitle.text = oneDog.breed
+                        binding.tvOtherName.text = oneDog.meta.other_names
+                        binding.tvNote.text = oneDog.meta.notes
+                        binding.tvNote.isVisible = false
+                    }
+                }catch(e:Throwable) {
+                    e.printStackTrace()
+                }
+            }
+        }
     }
 }
+
