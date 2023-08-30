b1eccde0ed13167366d7071a3d23373e81eeedd7
AnthonyPCPortable <anth06ny@gmail.com>
2023-08-30 14:35:56 +0200

    TP Correction Localisation

--- app/src/main/java/com/example/a23_07_aston/RequestUtils.kt
+++ app/src/main/java/com/example/a23_07_aston/RequestUtils.kt
@@ -4,13 +4,28 @@ import com.google.gson.Gson
 import okhttp3.OkHttpClient
 import okhttp3.Request
 
+fun main() {
+    val data = RequestUtils.loadWeather(43.568705, 1.489464)
+    println(data)
+}
+
 object RequestUtils {
+
     val client = OkHttpClient()
     val gson = Gson()
 
     fun loadWeather(city: String): WeatherBean {
 
         var json = sendGet("https://api.openweathermap.org/data/2.5/weather?q=$city&appid=b80967f0a6bd10d23e44848547b26550&units=metric&lang=fr")
+
+        val data  = gson.fromJson(json, WeatherBean::class.java)
+
+        return data
+    }
+
+    fun loadWeather(latitude:Double, longitude:Double): WeatherBean {
+
+        var json = sendGet("https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=b80967f0a6bd10d23e44848547b26550&units=metric&lang=fr")
 
         val data  = gson.fromJson(json, WeatherBean::class.java)
 
--- app/src/main/java/com/example/a23_07_aston/WeatherActivity.kt
+++ app/src/main/java/com/example/a23_07_aston/WeatherActivity.kt
@@ -1,10 +1,14 @@
 package com.example.a23_07_aston
 
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import com.example.a23_07_aston.databinding.ActivityWeatherBinding
 import com.squareup.picasso.Picasso
@@ -44,6 +48,68 @@ class WeatherActivity : AppCompatActivity() {
                 }
             }
         }
+
+        binding.btMyLoc.setOnClickListener {
+            //loadWeather()
+
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED
+            ) {
+                //On a la permission
+                println("j'ai la permission")
+                loadWeather()
+            } else {
+                println("Je la demande")
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 0
+                )
+            }
+        }
+
+    }
+
+    //callback de la popup de demande de permission
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            //On a la permission
+            loadWeather()
+        } else {
+            errorMessage = "Il faut la permission"
+            refreshScreen()
+        }
+    }
+
+    //Méthode uqi lance la recherche par localisation
+    fun loadWeather() {
+
+
+        binding.progressBar.isVisible = true
+        thread {
+            try {
+                val location : Location? = LocationUtils.getLastKnownLocation(this)
+                if(location == null) {
+                    throw Exception("Pas de localisation trouvée")
+                }
+
+                data = RequestUtils.loadWeather(location.latitude, location.longitude)
+                errorMessage = null
+                runOnUiThread {
+                    refreshScreen()
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                data = null
+                errorMessage = "Erreur lors du chargement des données météo : ${e.message}"
+                runOnUiThread {
+                    refreshScreen()
+                }
+            }
+        }
     }
 
     private fun refreshScreen() {
@@ -53,7 +119,8 @@ class WeatherActivity : AppCompatActivity() {
             binding.tvTexte.text = "Il fait ${data!!.main.temp}° à ${data!!.name} avec un vent de ${data!!.wind.speed} m/s"
             val urlImage = "https://openweathermap.org/img/wn/${data!!.weather[0].icon}@4x.png"
             Picasso.get().load(urlImage).into(binding.imageView)
-        } else if (!errorMessage.isNullOrEmpty()) {
+        }
+        if (!errorMessage.isNullOrEmpty()) {
             // Fait apparaître le message d'erreur avec une animation
             binding.tvError.text = errorMessage
             binding.tvError.visibility = View.VISIBLE
